485258efd92b80ed8f3c5a5a48f0c9cb329d5176
Trevor Alexander <talex@privatdemail.net>
2015-02-10 11:57:49 -1000

    string input sanitization

--- palindrome-detector.rkt
+++ palindrome-detector.rkt
@@ -1,9 +1,11 @@
 #lang racket
 
-(module palindrome-detector racket
 (provide detect-palindrome)
 
-(define (detect-palindrome inlist)
-  (if (equal? (reverse inlist) inlist) #t #f)
+(define (detect-palindrome infoo)
+  ;@ sanitize input
+  (define testlist (if (string? infoo) (string->list infoo) infoo))
+  
+  ;@ reverse, compare
+  (if (equal? (reverse testlist) testlist) #t #f)
   )
-)
