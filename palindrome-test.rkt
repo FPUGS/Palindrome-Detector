9393cb561e9f0ab971fbd2b83c84d8c2e8604656
Trevor Alexander <talex@privatdemail.net>
2015-02-10 16:40:43 -1000

    fancified tests

--- palindrome-test.rkt
+++ palindrome-test.rkt
@@ -3,12 +3,21 @@
 (require rackunit
          "palindrome-detector.rkt")
 
-(test-case "palindrome list of numbers is detected"
-           (check-true (detect-palindrome '(1 2 3 2 1))))
-(test-case "non-palindrome list of numbers is rejected"
-           (check-false (detect-palindrome '(1 2 3 4 5))))
 
-(test-case "palindrome list of attributes is detected"
-           (check-true (detect-palindrome [:foo :bar :foo])))
-(test-case "palindrome string is detected"
-           (check-true (detect-palindrome "racecar")))
+(define palindrome-tests
+  (test-suite "Palindrome detector tests"
+              (test-case "palindrome list of numbers is detected"
+                         (check-true (detect-palindrome '(1 2 3 2 1))))
+
+              (test-case "non-palindrome list of numbers is rejected"
+                         (check-false (detect-palindrome '(1 2 3 4 5))))
+
+              (test-case "palindrome list of expressions is detected"
+                         (let ([foo 5] [bar 6])
+                           (check-true (detect-palindrome '[foo bar foo]))))
+
+              (test-case "palindrome string is detected"
+                         (check-true (detect-palindrome "racecar")))))
+
+(require rackunit/text-ui)
+(run-tests palindrome-tests 'verbose)
